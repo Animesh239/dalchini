797b83b852dfcd015a567dd9698ef95e12f47e0f
Animesh239 <animeshbarik239@gmail.com>
2023-11-07 23:43:12 +0530

    Update router import and add RouterProvider component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import router from './router/index.js'
+import router from './router'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}  />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
