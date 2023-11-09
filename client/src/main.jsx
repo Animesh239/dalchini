75a7c5d22bdadd5503b59b0b0c6044c09bd395fb
Animesh239 <animeshbarik239@gmail.com>
2023-11-09 22:52:54 +0530

    Add font and styling imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,12 @@ import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import { Container } from "@mui/system";
 import './firebase/config.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
