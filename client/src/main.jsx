58b6d542cb6e6ff4f6de492fa44b6e5deef4de7e
Animesh239 <animeshbarik239@gmail.com>
2023-11-13 15:49:02 +0530

    Refactor code to improve performance and readability.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,18 +5,15 @@ import "./index.css";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import { Container } from "@mui/system";
-import './firebase/config.jsx'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-
+import "./firebase/config.jsx";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Container maxWidth='lg' sx={{textAlign : 'center' , mt: '50px'}}>
-      <RouterProvider router={router} />
-    </Container>
-  </React.StrictMode>
+  <Container maxWidth="lg" sx={{ textAlign: "center", mt: "50px" }}>
+    <RouterProvider router={router} />
+  </Container>
 );
